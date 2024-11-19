468a89cf27dceaa5f765cca2bae415cf30864ee9
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-19 14:08:08 +0900

    Feat : 코스, 장소, 투표 Entity 구현

---
+++ src/main/java/org/example/flowday/domain/course/entity/Course.java
@@ -0,0 +1,48 @@
+package org.example.flowday.domain.course.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.flowday.domain.spot.entity.Spot;
+import org.example.flowday.domain.vote.entity.Vote;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private String title;
+    private LocalDate date;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vote> votes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Spot> spots = new ArrayList<>();
+}
---
+++ src/main/java/org/example/flowday/domain/spot/entity/Category.java
@@ -0,0 +1,5 @@
+package org.example.flowday.domain.spot.entity;
+
+public enum Category {
+    CAFE, RESTAURENT, PLACE
+}
