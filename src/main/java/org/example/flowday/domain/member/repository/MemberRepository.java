bd8502b5c7cbe015a57cf26d7f8f6eaca664c09a
asklas <asklas+jyjy0372@gmail.com>
2024-11-19 12:34:41 +0900

    feat: Member 기본 CRUD 코드

---
+++ src/main/java/org/example/flowday/domain/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package org.example.flowday.domain.member.repository;
+
+import org.example.flowday.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByLoginId(String loginId); // 로그인 ID로 검색
+    Optional<Member> findByEmail(String email); // 이메일로 검색
+}
+
