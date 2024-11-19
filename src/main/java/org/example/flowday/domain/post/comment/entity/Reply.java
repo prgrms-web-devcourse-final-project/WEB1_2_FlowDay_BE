86448e97c43ff56a8f379517d68854da9b197b0b
seosang-won <zmdk1205@naver.com>
2024-11-19 12:41:47 +0900

    feat : Add Post , Tag , Reply entity

---
+++ src/main/java/org/example/flowday/domain/post/comment/entity/Reply.java
@@ -0,0 +1,46 @@
+package org.example.flowday.domain.post.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.flowday.domain.post.post.entity.Post;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Reply parent;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    private List<Reply> children = new ArrayList<>();
+
+
+}
