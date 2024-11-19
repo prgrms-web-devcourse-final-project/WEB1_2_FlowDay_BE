585bdd9b6deec3becb2c1ebd39cf1c0fd04f6b6b
asklas <asklas+jyjy0372@gmail.com>
2024-11-19 14:08:00 +0900

    feat: 아이디 존재 여부 확인 메서드 추가

--- src/main/java/org/example/flowday/domain/member/repository/MemberRepository.java
+++ src/main/java/org/example/flowday/domain/member/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Boolean existsByLoginId(String username);
     Optional<Member> findByLoginId(String loginId); // 로그인 ID로 검색
     Optional<Member> findByEmail(String email); // 이메일로 검색
 }
