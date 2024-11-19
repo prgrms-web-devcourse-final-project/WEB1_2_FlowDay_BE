4558575e729a7a9e56860eda676a2ae9675b7de3
seosang-won <zmdk1205@naver.com>
2024-11-19 11:15:50 +0900

    init

---
+++ src/test/java/org/example/flowday/FlowdayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flowday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
