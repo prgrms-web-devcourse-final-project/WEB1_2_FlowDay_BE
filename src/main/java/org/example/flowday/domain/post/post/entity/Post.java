a3b496c28115b33b28fd37b1115b7c1fe9bba2cc
seosang-won <zmdk1205@naver.com>
2024-11-19 12:41:47 +0900

    feat : Add Post , Tag , Reply entity

---
+++ src/main/java/org/example/flowday/domain/post/post/entity/Post.java
@@ -0,0 +1,56 @@
+package org.example.flowday.domain.post.post.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.flowday.domain.post.comment.entity.Reply;
+import org.example.flowday.domain.post.tag.entity.Tag;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String city;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member writer;
+
+    @OneToOne
+    private Course course;
+
+    @OneToMany(mappedBy = "post" , cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<Tag> tags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post" , cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<Reply> replys = new ArrayList<>();
+
+}
