446cefc8bb0a8948bcd63e256d747dfc906a35b5
asklas <asklas+jyjy0372@gmail.com>
2024-11-19 14:07:32 +0900

    feat: login 위한 SecurityUser 생성

---
+++ src/main/java/org/example/flowday/global/security/util/SecurityUser.java
@@ -0,0 +1,39 @@
+package org.example.flowday.global.security.util;
+
+import org.example.flowday.domain.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SecurityUser implements UserDetails {
+
+    private final Member member;
+
+    public SecurityUser(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole().toString();
+            }
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPw();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getLoginId();
+    }
+}
