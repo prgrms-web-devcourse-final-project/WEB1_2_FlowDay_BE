091c125e69aa9226cba71cbfc79f123d085e55da
asklas <asklas+jyjy0372@gmail.com>
2024-11-21 17:46:02 +0900

    mod: 사용하지 않는 에러 코드를 제거하고, 텍스트를 일부 간결하게 수정했습니다

--- src/main/java/org/example/flowday/domain/member/exception/MemberException.java
+++ src/main/java/org/example/flowday/domain/member/exception/MemberException.java
@@ -4,15 +4,10 @@ import org.springframework.http.HttpStatus;
 
 public enum MemberException {
     MEMBER_NOT_FOUND("존재하지 않는 ID입니다.", HttpStatus.NOT_FOUND),
-    MEMBER_NOT_REGISTERED("회원가입에 실패했습니다.", HttpStatus.BAD_REQUEST),
-    MEMBER_NOT_MODIFIED("회원정보 수정에 실패했습니다.", HttpStatus.BAD_REQUEST),
-    MEMBER_LOGIN_DENIED("로그인에 실패했습니다.", HttpStatus.BAD_REQUEST),
-    MEMBER_NOT_REMOVED("회원정보 삭제에 실패했습니다", HttpStatus.BAD_REQUEST),
-    MEMBER_IMAGE_NOT_MODIFIED("이미지 변경에 실패했습니다", HttpStatus.BAD_REQUEST),
+    MEMBER_IMAGE_NOT_MODIFIED("이미지 변경 실패", HttpStatus.BAD_REQUEST),
     MEMBER_IMAGE_NOT_SAVED("이미지 저장 실패", HttpStatus.BAD_REQUEST),
-
-    MEMBER_ACCESSTOKEN_EXPIRED("Access 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
-    MEMBER_REFRESHTOKEN_EXPIRED("Refresh 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED);
+    MEMBER_NAME_NOT_FOUND("해당 이름으로 회원을 찾을 수 없습니다", HttpStatus.NOT_FOUND),
+    MEMBER_EMAIL_NOT_FOUND("해당 이메일로 회원을 찾을 수 없습니다", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
