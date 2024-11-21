adc142a46da04c7828f923da2464a1d16d69f716
asklas <asklas+jyjy0372@gmail.com>
2024-11-21 17:47:35 +0900

    chore: 사용하지 않는 import문 삭제하였습니다

--- src/main/java/org/example/flowday/global/security/util/SecurityUserService.java
+++ src/main/java/org/example/flowday/global/security/util/SecurityUserService.java
@@ -2,7 +2,6 @@ package org.example.flowday.global.security.util;
 
 import org.example.flowday.domain.member.entity.Member;
 import org.example.flowday.domain.member.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
