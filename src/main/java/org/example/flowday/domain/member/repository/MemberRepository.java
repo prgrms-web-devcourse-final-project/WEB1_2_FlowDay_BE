b211073fb46f706f05ccae199d279a76b3787202
asklas <asklas+jyjy0372@gmail.com>
2024-11-21 10:11:20 +0900

    feat: 아이디/패스워드 찾기 기능 및 토큰 재발급을 위한 쿼리문 추가

--- src/main/java/org/example/flowday/domain/member/repository/MemberRepository.java
+++ src/main/java/org/example/flowday/domain/member/repository/MemberRepository.java
@@ -2,12 +2,22 @@ package org.example.flowday.domain.member.repository;
 
 import org.example.flowday.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsByLoginId(String username);
     Optional<Member> findByLoginId(String loginId); // 로그인 ID로 검색
-    Optional<Member> findByEmail(String email); // 이메일로 검색
+
+    @Query("SELECT m.refreshToken FROM Member m WHERE m.loginId = :loginId")
+    Optional<String> findRefreshTokenByLoginId(String loginId);
+
+    @Query("SELECT m.id FROM Member m WHERE m.loginId = :loginId")
+    Optional<Long> findIdByLoginId(String loginId);
+
+    @Query("SELECT m.loginId FROM Member m WHERE m.email = :email")
+    Optional<String> findLoginIdByEmail(String email);
+    Optional<Member> findByLoginIdAndEmail(String loginId , String email);
 }
 
