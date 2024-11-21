6a0fb0951dd06237c8150fe150e838b471346020
asklas <asklas+jyjy0372@gmail.com>
2024-11-21 10:14:25 +0900

    feat: Security 필터 순서 조정 및 설정. 시나리오는 모식도 작성 예정

--- src/main/java/org/example/flowday/global/security/config/SecurityConfig.java
+++ src/main/java/org/example/flowday/global/security/config/SecurityConfig.java
@@ -2,17 +2,23 @@ package org.example.flowday.global.security.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.flowday.domain.member.repository.MemberRepository;
+import org.example.flowday.global.security.filter.JwtFilter;
 import org.example.flowday.global.security.filter.LoginFilter;
+import org.example.flowday.global.security.util.JwtUtil;
+import org.example.flowday.global.security.util.oauth2.service.CustomOAuth2UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
@@ -26,17 +32,22 @@ public class SecurityConfig {
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    private final MemberRepository memberRepository;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil, CustomOAuth2UserService customOAuth2UserService, MemberRepository memberRepository) {
 
         this.authenticationConfiguration = authenticationConfiguration;
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.memberRepository = memberRepository;
     }
 
-    //AuthenticationManager Bean 등록
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-
         return configuration.getAuthenticationManager();
     }
+
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -44,7 +55,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtUtil jwtUtil) throws Exception {
 
         http
                 .csrf(csrf -> csrf
@@ -65,11 +76,23 @@ public class SecurityConfig {
                                 new AntPathRequestMatcher("/h2-console/**")
                         )
                         .permitAll()
-                        .requestMatchers("/api/members/login","/api/members").permitAll()
-                        .anyRequest().authenticated());
+                        .requestMatchers(
+                                "/api/members/login",
+                                "/api/members",
+                                "/oauth2/**"
+                        ).permitAll()
+                        .anyRequest().hasRole("USER"));
 
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, memberRepository), UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .addFilterBefore(new JwtFilter(jwtUtil, memberRepository), OAuth2AuthorizationRequestRedirectFilter.class);
+
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)));
 
         http
                 .headers(
