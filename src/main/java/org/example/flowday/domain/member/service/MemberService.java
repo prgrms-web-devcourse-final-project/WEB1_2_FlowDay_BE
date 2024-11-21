ecd4a428db73233fd48825e898c2672fede1328e
asklas <asklas+jyjy0372@gmail.com>
2024-11-21 10:12:48 +0900

    feat: 주석으로 상세 기술

--- src/main/java/org/example/flowday/domain/member/service/MemberService.java
+++ src/main/java/org/example/flowday/domain/member/service/MemberService.java
@@ -1,12 +1,21 @@
 package org.example.flowday.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.flowday.domain.member.dto.MemberDTO;
 import org.example.flowday.domain.member.entity.Member;
+import org.example.flowday.domain.member.entity.Role;
+import org.example.flowday.domain.member.exception.MemberException;
 import org.example.flowday.domain.member.repository.MemberRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
 import java.util.Optional;
 
 @Service
@@ -18,24 +27,38 @@ public class MemberService {
 
     // 회원 가입
     @Transactional
-    public Member createMember(Member member) {
+    public MemberDTO.CreateResponseDTO createMember(Member member) {
         member.setPw(passwordEncoder.encode(member.getPw()));
-        return memberRepository.save(member);
+        member.setRole(Role.ROLE_USER);
+        Member savedMember = memberRepository.save(member);
+        return new MemberDTO.CreateResponseDTO(
+                savedMember.getId(),
+                savedMember.getLoginId(),
+                savedMember.getEmail(),
+                savedMember.getName(),
+                savedMember.getPhoneNum()
+        );
     }
 
     // 회원 조회
-    public Optional<Member> getMember(Long id) {
-        return memberRepository.findById(id);
+    public MemberDTO.ReadResponseDTO getMember(Long id) {
+        try {
+            Member member = memberRepository.findById(id).get();
+            return new MemberDTO.ReadResponseDTO(
+                    member.getName(),
+                    member.getProfileImage(),
+                    member.getPartnerId(),
+                    member.getDateOfRelationshipStart(),
+                    member.getDateOfBirth()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Error getting member", e);
+        }
     }
 
     // 로그인 ID로 회원 조회
-    public Optional<Member> getMemberByLoginId(String loginId) {
-        return memberRepository.findByLoginId(loginId);
-    }
-
-    // 이메일로 회원 조회
-    public Optional<Member> getMemberByEmail(String email) {
-        return memberRepository.findByEmail(email);
+    public MemberDTO.FindIdResponseDTO getMemberByEmail(String email) {
+        return new MemberDTO.FindIdResponseDTO(memberRepository.findLoginIdByEmail(email).get());
     }
 
     // 회원 수정
@@ -56,5 +79,65 @@ public class MemberService {
     @Transactional
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+    }
+
+    //이미지 변경
+    @Transactional
+    public MemberDTO.ChangeImageResponseDTO changeProfileImage(Long id, MultipartFile imageFile){
+
+        Optional<Member> memberOp = memberRepository.findById(id);
+        if (memberOp.isPresent()) {
+            Member member = memberOp.get();
+            String fileName = saveImage(imageFile);
+            member.setProfileImage(fileName);
+            memberRepository.save(member);
+            return new MemberDTO.ChangeImageResponseDTO(member.getId(), imageFile.getOriginalFilename());
+        } else {
+            throw MemberException.MEMBER_IMAGE_NOT_MODIFIED.getMemberTaskException();
+        }
+    }
+
+    // 사용자가 보낸 이미지 파일 저장
+    private String saveImage(MultipartFile imageFile) { // MultipartFile로 변경
+        String uploadDir = "upload/";
+        try {
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath); // 디렉토리가 존재하지 않으면 생성
+            }
+
+            // 파일 이름 생성 및 저장
+            String ident = String.valueOf(System.currentTimeMillis()).substring(3, 10);
+            String fileName = ident + "_" + imageFile.getOriginalFilename();
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(imageFile.getInputStream(), filePath); // 실제 파일 저장
+
+            return fileName; // 저장된 파일 이름 반환
+        } catch (IOException e) {
+            throw MemberException.MEMBER_IMAGE_NOT_SAVED.getMemberTaskException();
+        }
+    }
+
+    // 비밀번호 찾기 시, 임시로 적용할 비밀번호 생성
+    @Transactional
+    public String setTemplatePassword(String loginId, String email) {
+
+        String CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+        Member member = memberRepository.findByLoginIdAndEmail(loginId, email).orElseThrow(MemberException.MEMBER_NOT_FOUND::getMemberTaskException);
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder tempPassword = new StringBuilder(6);
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(CHAR_SET.length());
+            tempPassword.append(CHAR_SET.charAt(index));
+        }
+
+        String templatePassword = tempPassword.toString();
+
+        member.setPw(passwordEncoder.encode(templatePassword));
+        memberRepository.save(member);
+
+        return templatePassword;
     }
 }
