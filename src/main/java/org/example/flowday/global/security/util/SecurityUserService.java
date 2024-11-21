10948b7c7f8e2c6768f9d0cbfd12352b9d36fe01
asklas <asklas+jyjy0372@gmail.com>
2024-11-21 10:15:32 +0900

    feat: SecurityContext 에 담을 유저 객체와 loadUserByUsername 메서드 오버라이드

--- src/main/java/org/example/flowday/global/security/util/SecurityUserService.java
+++ src/main/java/org/example/flowday/global/security/util/SecurityUserService.java
@@ -8,24 +8,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class SecurityUserService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-    public SecurityUserService(final MemberRepository memberRepository) {
+    public SecurityUserService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
-        if(memberRepository.existsByLoginId(loginId)) {
-
-            return new SecurityUser(memberRepository.findByLoginId(loginId).get());
-
+        System.out.println("Looking for user with loginId: " + loginId);
+        Optional<Member> member = memberRepository.findByLoginId(loginId);
+        if (member.isPresent()) {
+            System.out.println("find successful");
+            return new SecurityUser(member.get());
+        } else {
+            System.out.println("find failed");
+            throw new UsernameNotFoundException("User not found with loginId: " + loginId);
         }
-        return null;
     }
-
 }
