6ea205ec1f72134473fdf2015129cc0505351541
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-22 12:37:49 +0900

    mod: 엔티티 어노테이션 추가

--- src/main/java/org/example/flowday/domain/course/spot/entity/Spot.java
+++ src/main/java/org/example/flowday/domain/course/spot/entity/Spot.java
@@ -1,9 +1,11 @@
 package org.example.flowday.domain.course.spot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.example.flowday.domain.course.course.entity.Course;
 import org.example.flowday.domain.course.vote.entity.Vote;
+import org.example.flowday.domain.course.wish.entity.WishPlace;
 
 @Entity
 @Getter
@@ -15,14 +17,17 @@ public class Spot {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private double latitude;
-    private double longitude;
+    @NotBlank(message = "Place Id cannot be blank")
+    private String placeId;
 
-    @Enumerated(EnumType.STRING)
-    private Category category;
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "City cannot be blank")
+    private String city;
 
     private String comment;
+
     private int sequence;
 
     @ManyToOne
@@ -32,5 +37,33 @@ public class Spot {
     @ManyToOne
     @JoinColumn(name = "vote_id", nullable = true)
     private Vote vote;
+
+    @ManyToOne
+    @JoinColumn(name = "wish_place_id", nullable = true)
+    private WishPlace wishPlace;
+
+    public void changePlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public void changeCity(String city) {
+        this.city = city;
+    }
+
+    public void changeComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void changeSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public void removeVote() {
+        this.vote = null;
+    }
 }
 
