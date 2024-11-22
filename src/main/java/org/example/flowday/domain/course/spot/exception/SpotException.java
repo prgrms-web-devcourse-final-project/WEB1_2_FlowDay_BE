50a174e42dc8ca90c4377a44984898eb447aad88
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-22 14:07:45 +0900

    mod: spot 불필요 예외 제거

--- src/main/java/org/example/flowday/domain/course/spot/exception/SpotException.java
+++ src/main/java/org/example/flowday/domain/course/spot/exception/SpotException.java
@@ -2,8 +2,6 @@ package org.example.flowday.domain.course.spot.exception;
 
 public enum SpotException {
     NOT_FOUND("SPOT NOT_FOUND", 404),
-    NOT_CREATED("SPOT NOT_CREATED", 400),
-    NOT_UPDATED("SPOT NOT_UPDATED", 400),
     NOT_DELETED("SPOT NOT_DELETED", 400);
 
     private SpotTaskException spotTaskException;
