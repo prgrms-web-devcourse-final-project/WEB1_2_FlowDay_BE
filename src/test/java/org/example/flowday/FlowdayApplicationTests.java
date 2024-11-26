d4a318438b13018496b4a6bef8d2d12ee6da2e90
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-26 21:56:30 +0900

    Mod: 테스트 수정

--- src/test/java/org/example/flowday/FlowdayApplicationTests.java
+++ src/test/java/org/example/flowday/FlowdayApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.flowday;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlowdayApplicationTests {
 
     @Test
