cf5d3299f8b1f815783cbc4594169ca6e0fec7da
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-26 14:12:46 +0900

    Refactor: 예외 처리 변경

--- src/main/java/org/example/flowday/domain/course/course/exception/CourseException.java
+++ src/main/java/org/example/flowday/domain/course/course/exception/CourseException.java
@@ -1,18 +1,23 @@
 package org.example.flowday.domain.course.course.exception;
 
+import org.springframework.http.HttpStatus;
+
 public enum CourseException {
-    NOT_FOUND("COURSE NOT_FOUND", 404),
-    NOT_CREATED("COURSE NOT_CREATED", 400),
-    NOT_UPDATED("COURSE NOT_UPDATED", 400),
-    NOT_DELETED("COURSE NOT_DELETED", 400);
+    NOT_FOUND("코스를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_CREATED("코스를 생성하는 데 실패했습니다.", HttpStatus.BAD_REQUEST),
+    NOT_UPDATED("코스를 수정하는 데 실패했습니다.", HttpStatus.BAD_REQUEST),
+    NOT_DELETED("코스를 삭제하는 데 실패했습니다.", HttpStatus.BAD_REQUEST),
+    FORBIDDEN("권한이 없습니다.", HttpStatus.FORBIDDEN);
 
-    private CourseTaskException courseTaskException;
+    private final String message;
+    private final HttpStatus status;
 
-    CourseException(String message, int code) {
-        courseTaskException = new CourseTaskException(message, code);
+    CourseException(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
     }
 
     public CourseTaskException get() {
-        return courseTaskException;
+        return new CourseTaskException(this.message, this.status);
     }
 }
