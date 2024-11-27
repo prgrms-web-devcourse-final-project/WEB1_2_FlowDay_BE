59e1eedb209a89d1a1d93657babb2524c1be068f
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-27 17:39:25 +0900

    Add: post 예외 처리 1차

---
+++ src/test/java/org/example/flowday/domain/post/post/service/PostServiceTest.java
@@ -0,0 +1,32 @@
+package org.example.flowday.domain.post.post.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void findAllCouplePosts() {
+    }
+
+    @Test
+    void findAllPrivate() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+}
