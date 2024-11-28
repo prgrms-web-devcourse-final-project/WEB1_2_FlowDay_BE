8ff48cfdf65154c753e4fc93d76d29e5944fca02
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-28 22:36:55 +0900

    Mod: 순서 제거

--- src/main/java/org/example/flowday/global/initData/NotProd.java
+++ src/main/java/org/example/flowday/global/initData/NotProd.java
@@ -104,21 +104,18 @@ public class NotProd {
                 .placeId("place1")
                 .name("카페")
                 .comment("코멘트")
-                .sequence(1)
                 .build();
         SpotReqDTO spot2 = SpotReqDTO.builder()
                 .city("서울 종로")
                 .placeId("place2")
                 .name("밥집")
                 .comment("코멘트")
-                .sequence(2)
                 .build();
         SpotReqDTO spot3 = SpotReqDTO.builder()
                 .city("서울 종로")
                 .placeId("place3")
                 .name("영화관")
                 .comment("코멘트")
-                .sequence(3)
                 .build();
 
         List<SpotReqDTO> spots = new ArrayList<>();
