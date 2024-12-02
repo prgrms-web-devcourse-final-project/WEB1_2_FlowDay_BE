1a359c4f795be3a2b7ff2097d7df5bfb6ae589ae
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-12-02 16:45:52 +0900

    chore : 게시글,댓글 swagge 문서 추가 (#56)

--- src/main/java/org/example/flowday/domain/post/likes/controller/LikeController.java
+++ src/main/java/org/example/flowday/domain/post/likes/controller/LikeController.java
@@ -19,14 +19,14 @@ public class LikeController {
     private final LikeService likeService;
 
     // 좋아요 추가
-    @Operation(summary = "게시글 좋아요 생성")
+    @Operation(summary ="게시글 좋아요 추가 "  , description = "이미 좋아요를 눌렀으면 예외가 발생합니다 ")
     @PostMapping("{postId}")
     public ResponseEntity<LikesDTO.LikeResponseDTO> addLike(@PathVariable Long postId , @AuthenticationPrincipal SecurityUser user) {
         return ResponseEntity.ok(likeService.addLike(postId, user.getId()));
     }
 
     // 좋아요 삭제
-    @Operation(summary = "게시글 좋아요 삭제")
+    @Operation(summary ="게시글 좋아요 삭제 "  , description = "이미 좋아요를 삭제했어도 예외 처리는 발생하지 않으나 게시글의 좋아요는 음수로 내려가지 않습니다 ")
     @DeleteMapping("{postId}")
     public ResponseEntity<String> removeLike(@PathVariable Long postId , @AuthenticationPrincipal SecurityUser user) {
         likeService.removeLike(postId, user.getId());
