fa57fe62b9819c4ced36f2d1b0aa2ed67c7d2e38
kang-ye-jin <yejin331@gachon.ac.kr>
2024-12-09 14:57:50 +0900

    refactor : 쿼리 수정

--- src/main/java/org/example/flowday/domain/course/course/repository/CourseRepository.java
+++ src/main/java/org/example/flowday/domain/course/course/repository/CourseRepository.java
@@ -13,15 +13,14 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findAllByMemberIdAndStatus(Long memberId, Status status);
 
     @Query("""
-    SELECT c.id, c.member.id, c.title, c.status, c.date, c.color, 
-            GROUP_CONCAT(s.placeId), GROUP_CONCAT(s.sequence)
+    SELECT c, COALESCE(GROUP_CONCAT(s.placeId), '')
     FROM Course c
     LEFT JOIN c.spots s
-    WHERE (c.member.id = :memberId OR c.member.id = :partnerId)
-    AND c.status = :status
+    WHERE (c.member.id = :memberId
+           OR (c.member.id = :partnerId AND c.status = 'COUPLE' AND :partnerId IS NOT NULL))
     GROUP BY c.id
     ORDER BY c.createdAt DESC
     """)
-    List<Object[]> findAllByMemberIdOrPartnerId(Long memberId, Long partnerId, Status status);
+    List<Object[]> findAllByMemberIdOrPartnerId(Long memberId, Long partnerId);
 
 }
